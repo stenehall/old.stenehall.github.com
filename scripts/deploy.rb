20f6c4b173b5460432aa1a037a7798055f4a6c5e
stenehall <stenehall@gmail.com>
2010-12-30 23:07:03 +0100

    Adding deploy script, minified css and atom

---
+++ scripts/deploy.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+
+css = ""
+Dir.glob("../media/css/*.css") do |file|
+   css+=IO.read(file)
+end
+cssfile = File.new("../media/css/combined.css", "w")
+cssfile.write(css)
+cssfile.close
+`yuicompressor -o ../media/css/minified.css ../media/css/combined.css`
+
+
